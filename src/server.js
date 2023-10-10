e807aa42456aa337e8eb546c1e1866105ee56aa6
arthur <arthur.rios007@gmail.com>
2023-10-09 22:26:24 -0300

    fix cors origin

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
-    withCredentials: true 
+    withCredentials: 'true' 
   }))
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
