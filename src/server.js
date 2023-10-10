4420caf4f266742c224d52beb672b8f47eb9b615
arthur <arthur.rios007@gmail.com>
2023-10-09 22:34:34 -0300

    fix cors origin

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://foodexplorer-r2.netlify.app' ,'http://localhost:5173'],
     credentials: true 
   }))
 
